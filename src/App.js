ba9e5eb83e3df4be218afae73059a17390c5bd1e
PeterRegas <55000513+PeterRegas@users.noreply.github.com>
2023-06-16 14:23:13 -0400

    Home page done nav bar done work started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Work from "./components/Work";
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Home/>
+      <Work/>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Peter Regas
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          I'm a Software Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+        I’m a new graduate with experiance in a wide array of development including full stack web development, mobile app development, and Unity 2d/3d game development.
+        Some of my favorite things I learned in school were ai aplications, scientific data analisis, computer vision and quantum computing.
+        I am highly motivated and a fast learner looking to bring value wherever I work.
+        </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            View Work
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

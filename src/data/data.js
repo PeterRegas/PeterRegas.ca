607da04ed47c47166960f914230dcfd829fc86bc
PeterRegas <55000513+PeterRegas@users.noreply.github.com>
2023-06-19 15:07:41 -0400

    update projects

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,7 @@
 import ttd from '../assets/ttd.png'
+import wordle from '../assets/wordle.jpg'
+import tobo from '../assets/tobo.jpg'
+import tophat from '../assets/tophat.png'
 
 export const data=[
     {
@@ -11,40 +14,26 @@ export const data=[
 
     {
         id:2,
-        name:"Titan Tower Defense",
-        image:ttd,
-        github:"https://github.com/PeterRegas/TitanTowerDefense",
-        demo:"https://www.youtube.com/watch?v=GWPO7aeoGss",
+        name:"Tophat Survivor",
+        image:tophat,
+        github:"https://github.com/PeterRegas/TopHatSurvivor",
+        demo:"https://www.youtube.com/watch?v=L_wKFejtyyo",
     },
     {
         id:3,
-        name:"Titan Tower Defense",
-        image:ttd,
-        github:"https://github.com/PeterRegas/TitanTowerDefense",
-        demo:"https://www.youtube.com/watch?v=GWPO7aeoGss",
+        name:"Wordle Clone",
+        image:wordle,
+        github:"https://github.com/PeterRegas/WordleClone",
+        demo:"",
     },
 
     {
         id:4,
-        name:"Titan Tower Defense",
-        image:ttd,
-        github:"https://github.com/PeterRegas/TitanTowerDefense",
-        demo:"https://www.youtube.com/watch?v=GWPO7aeoGss",
-    },
-    {
-        id:5,
-        name:"Titan Tower Defense",
-        image:ttd,
-        github:"https://github.com/PeterRegas/TitanTowerDefense",
-        demo:"https://www.youtube.com/watch?v=GWPO7aeoGss",
+        name:"Toboggan Hill Finder/Review App",
+        image:tobo,
+        github:"https://github.com/PeterRegas/TobogganApp",
+        demo:"",
     },
 
-    {
-        id:6,
-        name:"Titan Tower Defense",
-        image:ttd,
-        github:"https://github.com/PeterRegas/TitanTowerDefense",
-        demo:"https://www.youtube.com/watch?v=GWPO7aeoGss",
-    },
 
 ]
